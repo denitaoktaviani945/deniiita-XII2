da3f91a6cbe0b3d15161d2ad93ca26d23341fb6a
denitaa <khoerulrozakkafi@gmail.com>
2022-02-16 11:15:03 +0700

    denita

---
+++ src/components/button.js
@@ -0,0 +1,19 @@
+import { Children } from "react/cjs/react.production.min";
+
+export default function Button( {children, variant} ) 
+
+const variant = {
+  ""
+}
+
+{
+    return (
+        <div className="w-3/12">
+        <div className="button">
+          <button className="border-2 rounded-full px-6 border-white bg-white text-rose-600 font-semibold  mx-32">
+            {children}
+          </button>
+        </div>
+        </div>
+    )
+}
